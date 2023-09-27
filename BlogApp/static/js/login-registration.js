b43eaac0e24d993596d3962f694e878d29caeeb1
vickouma77 <Vickprestone20@gmail.com>
2023-09-27 22:14:08 +0300

    fetch()

--- BlogApp/static/js/login-registration.js
+++ BlogApp/static/js/login-registration.js
@@ -9,3 +9,60 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+// Register a user
+const signUpForm = document.querySelector('.sign-up-container form');
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // Get user info
+    const name = signUpForm.querySelector('input[type="text"]').value;
+    const email = signUpForm.querySelector('input[type="email"]').value;
+    const password = signUpForm.querySelector('input[type="password"]').value;
+
+    try {
+        const response = await fetch('http://localhost:5000/api/auth/register', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            window.location = '../home-page.html';
+            alert('Sign up successful');
+        } else {
+            const data = await response.json(); // Get error message from the server
+            alert(`Failed to sign up: ${data.message}`);
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+});
+
+// Login a user
+const signInForm = document.querySelector('.sign-in-container form');
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // Get user info
+    const email = signInForm.querySelector('input[type="email"]').value;
+    const password = signInForm.querySelector('input[type="password"]').value;
+
+    try {
+        const response = await fetch('http://localhost:5000/api/auth/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            window.location = '../home-page.html'; // Redirect to the dashboard
+            alert('Login successful');
+        } else {
+            const data = await response.json(); // Get error message from the server
+            alert(`Failed to login: ${data.message}`);
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+});
