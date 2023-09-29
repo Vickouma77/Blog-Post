5f320ac77b33a93e0e1495b64b74e60b7e11c397
vickouma77 <Vickprestone20@gmail.com>
2023-09-29 16:26:36 +0300

    cleaning up comments

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ const userRoute = require('./routes/user');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
 
-// Load environment variables from the .env file into process.env
 dotenv.config();
 
 const corsOptions = {
@@ -22,10 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
-
-// Serve static files
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 /*Connect to MongoDB */
@@ -33,7 +29,6 @@ mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        /*useCreateIndex: true,*/
     })
     .then(() => {
         console.log("Connected to MongoDB");
@@ -64,7 +59,7 @@ app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
 // Listen on port 5000
-const PORT = process.env.PORT || 5000; // Use the PORT from environment variables, or default to 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Backend is running on port ${PORT}`);
 });
