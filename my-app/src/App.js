3082993ad1ae8c52896865aabf71aed2c342f177
vickouma77 <Vickprestone20@gmail.com>
2023-10-03 15:38:26 +0300

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useCallback, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home ";
+import Blogs from "./components/Blogs";
+import Write from "./components/Write";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Blog from "./components/Blog";
+import Saved from "./components/Saved";
+import YourArticles from "./components/YourArticles";
 
 function App() {
+  const [userName, setUserName] = useState();
+  const getUser = useCallback(async (token) => {
+    const res = await fetch("http://localhost:8999", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "x-access-token": token,
+      },
+    });
+  
+    if (res.ok) {
+      const { firstName } = await res.json();
+      setUserName(firstName);
+      // localStorage.setItem("firstName", firstName);
+    }
+  }, []);
+  
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token2")); // set your token here
+    getUser(token);
+  }, [getUser]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blogs/:id" element={<Blog />} />
+        <Route path={`/${userName}/savedArticles`} element={<Saved />} />
+        <Route path='/yourarticles' element={<YourArticles />} />
+      </Routes>
+    </>
   );
 }
 
