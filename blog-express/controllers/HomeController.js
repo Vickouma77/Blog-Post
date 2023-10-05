7b9fb168a483bf83b34ece9808ea767ef78ca1d1
vickouma77 <Vickprestone20@gmail.com>
2023-10-05 13:43:07 +0300

    addressing jwt malformed error

--- blog-express/controllers/HomeController.js
+++ blog-express/controllers/HomeController.js
@@ -1,20 +1,36 @@
 import jwt from "jsonwebtoken";
-import * as dotenv from "dotenv";
-dotenv.config();
+import dotenv from "dotenv";
 import RegisterSchema from "../Models/RegisterModel.js";
+
+dotenv.config();
 
 export const HomeGet = async (req, res) => {
   const token = req.headers["x-access-token"];
-//   console.log(token);
-  try {
-  	const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  	const email = decoded.email
-  	const user = await RegisterSchema.findOne({ email: email })
 
-    // console.log('decoded-email => ', decoded.email);
-  	return res.status(200).json({ status: 'ok', firstName: user.firstName })
+  try {
+    if (!token) {
+      return res.status(401).json({ status: 'error', message: 'Token is missing' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const email = decoded.email;
+
+    if (!email) {
+      return res.status(401).json({ status: 'error', message: 'Invalid token' });
+    }
+
+    const user = await RegisterSchema.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ status: 'error', message: 'User not found' });
+    }
+
+    return res.status(200).json({ status: 'ok', firstName: user.firstName });
   } catch (error) {
-  	console.log(error)
-  	res.json({ status: 'error', error: 'invalid token' })
+    console.error('JWT verification error:', error);
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ status: 'error', message: 'Token has expired' });
+    }
+    return res.status(401).json({ status: 'error', message: 'Invalid token' });
   }
 };
